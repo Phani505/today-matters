f1ce7580033064b2c3a6b75bdfc4acf9ec07c429
ansuman273 <ansuman273@gmail.com>
2018-11-02 17:10:49 +0530

    initial commit

---
+++ ui/banner/banner.controller.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('BannerController', BannerController);
+
+    BannerController.$inject = ['$location'];
+    function BannerController($location) {
+        var vm = this;
+
+        vm.banner = banner;
+
+        function banner() {
+            vm.dataLoading = true;
+        };
+    }
+
+})();
